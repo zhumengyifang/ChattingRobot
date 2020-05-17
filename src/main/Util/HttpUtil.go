b42a5f34552c6db5d3f5f15adc6d1aeb6d94688e
Jarvis <>
2020-05-17 15:36:15 +0800

    httpUtil

--- src/main/Util/HttpUtil.go
+++ src/main/Util/HttpUtil.go
@@ -10,13 +10,13 @@ import (
 func HttpPost(json string) string {
 	jsonStr := []byte(json)
 	req, err := http.NewRequest("POST", model.Url, bytes.NewBuffer(jsonStr))
-	if err!=nil{
+	if err != nil {
 		panic(err.Error())
 	}
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 
-	c:=make(chan string)
+	c := make(chan string)
 	defer close(c)
 	go func() {
 		resp, err := client.Do(req)
@@ -28,7 +28,7 @@ func HttpPost(json string) string {
 		if !(resp.StatusCode == 200) {
 			panic("Get Text Error")
 		}
-		c<-string(body)
+		c <- string(body)
 	}()
 	return <-c
 }
